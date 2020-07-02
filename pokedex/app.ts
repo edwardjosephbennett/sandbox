755476fdacb93de80efa941c7fafabde3e6a6e3a
Ed Bennett <ed.bennett@adtrak.co.uk>
2020-07-02 21:14:57 +0100

    feat: further tailwindcss setup

--- pokedex/app.ts
+++ pokedex/app.ts
@@ -1,6 +1,6 @@
 import * as P from './src/app';
 
 let appContainer: HTMLElement | any = document.querySelector('#app');
-console.log(P.config);
+// console.log(P.config);
 
-P.getPokemon(P.config.pokeapi.all);
+// P.getPokemon(P.config.pokeapi.all);
